6c6eadff868af97dfa9bb5b2c217b987e1ee5dfe
Nick Schuch <nick@myschuch.com>
2015-10-17 19:34:55 +1000

    Use url.URL

--- marco-lib.go
+++ marco-lib.go
@@ -1,33 +1,34 @@
 package marco
 
 import (
-        "bytes"
-        "encoding/json"
-        "net/http"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/url"
 )
 
 type Backend struct {
-        Type   string
-        Domain string
+	Type   string
+	Domain string
 	Weight int
-        List   []string
+	List   []*url.URL
 }
 
 func Send(backends []Backend, url string) error {
-        b, err := json.Marshal(backends)
-        if err != nil {
-                return err
-        }
+	b, err := json.Marshal(backends)
+	if err != nil {
+		return err
+	}
 
-        req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
-        req.Header.Set("Content-Type", "application/json")
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
+	req.Header.Set("Content-Type", "application/json")
 
-        client := &http.Client{}
-        resp, err := client.Do(req)
-        if err != nil {
-                return err
-        }
-        defer resp.Body.Close()
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-        return nil
+	return nil
 }
